50c9dcd90660edcd27373328d2dfb6b9ab674176
Gal Ben Haim <gal.benhaim@solaredge.com>
2019-10-22 22:53:02 +0300

    add instructions for setCurrentUser

--- js/modules/user/user.service.js
+++ js/modules/user/user.service.js
@@ -4,13 +4,22 @@ class UserService {
     this.state = state;
   }
   /**
-   * @return userModel { name: string, id: string }
+   * @return array of userModel { name: string, id: string }
    */
   fetchUsers() {
     this.state.users = [{ id: '0', name: 'gal' }, {id: '1', name: 'eyal'}];
   }
 
   setCurrentUser(currentUser) {
+    /**
+     * Set the current user
+     * 1. check if currentUser is already set
+     * 2. if not set the first user
+     * 3. if it is set set the current user to be the next on users list
+     * 4. test this function to make sure it returns to the first user after cycle is complete
+     */
+
+    // this is temporary (should set it to the real current user by above logic)
     this.state.currentUser = currentUser;
   }
 
